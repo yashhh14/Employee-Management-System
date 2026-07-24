b9a912a46d3653a8552a08ce3bf19b999adc8174
yashhhh14 <yashwanthsunny4362@gmail.com>
2026-07-24 19:40:15 +0530

    Fix Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 const express = require('express')
+require("dotenv").config();
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 const cors = require('cors')
 app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    credentials: true
-}))
+app.use(cors())
 // db call
 const connectDB = require('./db/connectDB.js')
 const auth = require('./controllers/auth.js')
@@ -16,5 +13,5 @@ connectDB()
 
 app.use('/',auth)
 app.listen(PORT,()=>{
-    console.log(`server is running in http://localhost${PORT}`);
+    console.log(`server is running in ${PORT}`);
 })
